5c90cb00ddbbe883806ac2537cc64377b97adec5
centinela1024tb <centinela1024tb@gmail.com>
2022-07-01 18:42:40 -0500

    [ADD] Se hace la condicional si pasa la validacion del prespuesto se muestroria e componente d ela aplicacion
    => Se crea componente isValidPresupuesto donde se valida si es valida seria true si no false

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,26 @@
 import NuevoPresupuesto from './NuevoPresupuesto';
 
-const Header = ({presupuesto, setPresupuesto}) => {
+const Header = ({presupuesto, 
+  setPresupuesto, 
+  isValidPresupuesto, 
+  setIsValidPresupuesto
+}) => {
   return (
     <header>
-      <h1>Planificador de Gastos</h1>   
+      <h1>Planificador de Gastos</h1>
 
-      <NuevoPresupuesto
-        presupuesto={presupuesto}
-        setPresupuesto={setPresupuesto}
-      />   
+      { isValidPresupuesto 
+          ? (<p>Control Presupuesto</p>)
+          : (
+              <NuevoPresupuesto
+                presupuesto={presupuesto}
+                setPresupuesto={setPresupuesto}
+                setIsValidPresupuesto={setIsValidPresupuesto}
+              /> 
+            )
+      }
+
+        
     </header>
   )
 }
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,18 +1,28 @@
 import { useState } from 'react'
 import Mensaje from './Mensaje'
 
-const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+const NuevoPresupuesto = ({
+    presupuesto, 
+    setPresupuesto,     
+    setIsValidPresupuesto
+}) => {
 
   const [mensaje, setMensaje] = useState('');
 
   const handlePresupuesto = (e) => {
     e.preventDefault();
 
-    if(!Number(presupuesto) || Number(presupuesto) < 0 ) {
+    if(!presupuesto || presupuesto < 0 ) {
       setMensaje('No es un presupuesto válido')
-    } else {
-      console.log('Si es un presupuesto válido')
-    }
+
+      // para que termine la funcion si el presupuesto no es valido
+      return;
+    } 
+
+    setMensaje('');
+    setIsValidPresupuesto(true);
+
+    
 
   }
 
@@ -26,10 +36,10 @@ const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
           <label htmlFor="">Definir Presupuesto</label>
           <input 
             className="nuevo-presupuesto"
-            type="text"
+            type="number"
             placeholder="Añade tu Presupuesto"
             value={presupuesto}
-            onChange={ e => setPresupuesto(e.target.value)}
+            onChange={ e => setPresupuesto(Number(e.target.value))}
           />
         </div>
         <input 
