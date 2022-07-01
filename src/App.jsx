42c0e615b5494ae02bddd4925458ce1583d48b9e
centinela1024tb <centinela1024tb@gmail.com>
2022-07-01 18:03:14 -0500

    [ADD] Se crea el state presupuesto en App.jsx
    => Y se pasa a Header presupuesto y setPresupuesto por props
    => Y de Header se vuleven a pasar presupuesto y setPresupuesto por props

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,16 @@ import { useState } from 'react';
 import Header from './components/Header';
 
 
-function App() {  
+function App() {
+
+  const [presupuesto, setPresupuesto] = useState(0);
 
   return (
     <div>
-      <Header/>
+      <Header
+        presupuesto={presupuesto}
+        setPresupuesto={setPresupuesto}
+      />
     </div>
   )
 }
--- src/components/NuevoPresupuesto.jsx
+++ src/components/NuevoPresupuesto.jsx
@@ -1,6 +1,6 @@
 
 
-const NuevoPresupuesto = () => {
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra">
       <form className="formulario">
@@ -10,6 +10,8 @@ const NuevoPresupuesto = () => {
             className="nuevo-presupuesto"
             type="text"
             placeholder="Añade tu Presupuesto"
+            value={presupuesto}
+            onChange={ e => setPresupuesto(e.target.value)}
           />
         </div>
         <input 
