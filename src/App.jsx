4dc1e2533c55ae64322c69db0648cf095c21823a
rvaldez20 <rvaldez20@gmail.com>
2022-07-12 13:21:31 -0500

    [ADD] Se pasan los gastos(state) hasta el componente ControlPresupuesto

--- src/App.jsx
+++ src/App.jsx
@@ -8,13 +8,13 @@ import IconoNuevoGasto from './img/nuevo-gasto.svg';
 
 function App() {
 
+  const [gastos, setGastos] = useState([])
+
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false);
   
   const [modal, setModal] = useState(false);
   const [animarModal, setAnimarModal] = useState(false)
-
-  const [gastos, setGastos] = useState([])
 
   const handleNuevoGasto =() => {
     setModal(true);
@@ -37,8 +37,9 @@ function App() {
   }
 
   return (
-    <div className={modal && 'fijar'}>
+    <div className={modal ? 'fijar' : ''}>
       <Header
+        gastos={gastos}
         presupuesto={presupuesto}
         setPresupuesto={setPresupuesto}
         isValidPresupuesto={isValidPresupuesto}
