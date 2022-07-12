8de1f8674363e34e87e489b9407d83108d0b5942
centinela1024tb <centinela1024tb@gmail.com>
2022-07-12 15:53:36 -0500

    [ADD] Se implementa la grafica, se importan los componentes y los estilos, falta configurarla con los datos del presupuesto y lo gastado

--- src/components/ControlPresupuesto.jsx
+++ src/components/ControlPresupuesto.jsx
@@ -1,4 +1,6 @@
 import { useState, useEffect } from 'react'
+import { CircularProgressbar } from 'react-circular-progressbar'
+import 'react-circular-progressbar/dist/styles.css';
 import {formatearCantidad} from '../helpers'
 
 const ControlPresupuesto = ({ gastos, presupuesto }) => { 
@@ -26,7 +28,9 @@ const ControlPresupuesto = ({ gastos, presupuesto }) => {
   return (
     <div className="contenedor-presupuesto contenedor sombra dos-columnas">
       <div>
-        <p>Grafica Aqui</p>
+        <CircularProgressbar 
+          value={50}
+        />
       </div>
       <div className="contenido-presupuesto">
         <p>
